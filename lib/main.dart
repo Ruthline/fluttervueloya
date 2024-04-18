f69bc7d59841cb394ba2ada6bf299c9ff6fbd50f
Ruthline <rutheveline@hotmail.es>
2024-04-17 23:07:53 -0500

    Navbar model interaction with parts of mobile app

--- lib/main.dart
+++ lib/main.dart
@@ -24,25 +24,50 @@ class MyFirstWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {//build es el encargado de mostrar widgets, ya sea por metodo o por clase, etc.
-    return Container(
-      color: Colors.pink,
-      child: Padding(
-        padding: const EdgeInsets.all(40),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        title: const Text ('VueloYA'),
+        actions: const [
+          Padding(padding: EdgeInsets.all(5), child: Icon(Icons.person),),
+          Padding(padding: EdgeInsets.only(right: 20), child: Icon(Icons.login),)
+
+          ]
+      ),
+
+      drawer: Drawer(
         child: Container(
-          color: Colors.lightBlue,
-          child: Padding(
-            padding: const EdgeInsets.all(50),
-            child: Container(
-              color: Colors.lightGreen,
-              child:Padding(
-                padding: const EdgeInsets.only(top: 255, bottom:255, left: 40, right: 40), child: Container(
-                  color: Colors.amber,
-                  ),
-              )
-            ),
-          ),
+          color: Colors.blueAccent,
+          child: const Center(child: Text ('Drawer Button'),),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        print('Jelou Print');
+      }, child: const Icon(Icons.call)),
+      body: Center(
+        child:AspectRatio(
+          aspectRatio: 1.0,
+          child: Container(
+          color: Colors.pink,
+          child: Padding(
+            padding: const EdgeInsets.all(40),
+            child: Container(
+              color: Colors.lightBlue,
+              child: Padding(
+                padding: const EdgeInsets.all(50),
+                child: Container(
+                  color: Colors.lightGreen,
+                  child:Padding(
+                    padding: const EdgeInsets.only(top: 255, bottom:255, left: 40, right: 40), child: Container(
+                      color: Colors.amber,
+                      ),
+                  ),
+                )),
+              ),
+            ),
+            ),
         )
+      ),
     );
   }
 }
